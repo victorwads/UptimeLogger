82d619e47457ad2730bd08770ac92f80fe331b28
Victor Wads <victor.laureano@gmail.com>
2023-04-09 17:45:29 -0300

    MacOs Empty App Projetct

---
+++ UptimeLoggerMacOs/UptimeLogger/UptimeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UptimeLoggerApp.swift
+//  UptimeLogger
+//
+//  Created by Victor Wads on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UptimeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
