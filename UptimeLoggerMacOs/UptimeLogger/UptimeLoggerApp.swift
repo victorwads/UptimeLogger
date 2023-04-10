4cecb1be5f406190affe03d538ffb0bee8dd91e9
Victor Wads <victor.laureano@gmail.com>
2023-04-09 23:06:32 -0300

    chore(MacApp): Adding log folder actions

--- UptimeLoggerMacOs/UptimeLogger/UptimeLoggerApp.swift
+++ UptimeLoggerMacOs/UptimeLogger/UptimeLoggerApp.swift
@@ -12,6 +12,36 @@ struct UptimeLoggerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.commands {
+            Menus()
+        }
+    }
+}
+
+struct Menus: Commands {
+    var body: some Commands {
+        CommandMenu("Logs") {
+            Button(action: {
+                let openPanel = NSOpenPanel()
+                openPanel.canChooseDirectories = true
+                openPanel.canChooseFiles = false
+                openPanel.allowsMultipleSelection = false
+                openPanel.directoryURL = URL(fileURLWithPath: LogsProvider.folder)
+
+                openPanel.begin { result in
+                    if result == .OK, let url = openPanel.url {
+                        LogsProvider.folder = url.relativePath+"/"
+                    }
+                }
+            }) {
+                Text("Change logs folder")
+            }
+            Button(action: {
+                let logsFolderURL = URL(fileURLWithPath: LogsProvider.folder)
+                NSWorkspace.shared.open(logsFolderURL)
+            }) {
+                Text("Open Logs Folder")
+            }
         }
     }
 }
