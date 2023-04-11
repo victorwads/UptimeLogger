3a735412152d3b5ee86ac5772e2eb27f1ba75eb4
Victor Wads <victor.laureano@gmail.com>
2023-04-11 00:11:34 -0300

    fix(App): bookmark after lose permissions

--- Source/Providers/FilesProvider.swift
+++ Source/Providers/FilesProvider.swift
@@ -8,69 +8,70 @@ import Cocoa
 import Foundation
 
 func bookmarkKey(_ path: String) -> String{
-  return "bm2:\(path)"
+    return "bm2:\(path)"
 }
 
 func bookmarkKey(url: URL) -> String{
-  return bookmarkKey(url.path)
+    return bookmarkKey(url.path)
 }
 
 class FilesProvider {
-  public static let shared = FilesProvider()
-  
+    public static let shared = FilesProvider()
+    
     func authorize(_ path: String, _ change: Bool, callback: @escaping (String) -> Void){
-    if !change {
-        if let bookmarkData = UserDefaults.standard.object(forKey: bookmarkKey(path)){
-            if self.resolveBookmark(data: bookmarkData as! Data){
-                callback(path)
-                return
+        if !change {
+            if let bookmarkData = UserDefaults.standard.object(forKey: bookmarkKey(path)){
+                if self.resolveBookmark(data: bookmarkData as! Data){
+                    callback(path)
+                    return
+                }
+            }
+        }
+        
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = false
+        openPanel.canChooseFiles = false
+        openPanel.directoryURL = URL(fileURLWithPath: path)
+        openPanel.message = Strings.providerFilesOpenMessage.value
+        openPanel.showsHiddenFiles = true
+        openPanel.begin { (result) -> Void in
+            if result == NSApplication.ModalResponse.OK {
+                let url = openPanel.urls.first!
+                do{
+                    let bookmarkData = try url.bookmarkData(options: .withSecurityScope, includingResourceValuesForKeys: nil, relativeTo: nil)
+                    UserDefaults.standard.setValue(bookmarkData, forKey: bookmarkKey(url: url))
+                    self.resolveBookmark(data: bookmarkData)
+                    callback(url.path)
+                } catch {
+                    print(error.localizedDescription)
+                }
             }
         }
     }
     
-    let openPanel = NSOpenPanel()
-    openPanel.allowsMultipleSelection = false
-    openPanel.canChooseDirectories = true
-    openPanel.canCreateDirectories = false
-    openPanel.canChooseFiles = false
-    openPanel.directoryURL = URL(fileURLWithPath: path)
-    openPanel.message = Strings.providerFilesOpenMessage.value
-    openPanel.showsHiddenFiles = true
-    openPanel.begin { (result) -> Void in
-      if result == NSApplication.ModalResponse.OK {
-        let url = openPanel.urls.first!
+    func resolveBookmark(data: Data) -> Bool{
         do{
-          let bookmarkData = try url.bookmarkData(options: .withSecurityScope, includingResourceValuesForKeys: nil, relativeTo: nil)
-          UserDefaults.standard.setValue(bookmarkData, forKey: bookmarkKey(url: url))
-          self.resolveBookmark(data: bookmarkData)
-          callback(url.path)
+            var isStale = ObjCBool(false)
+            let url = try NSURL(resolvingBookmarkData: data, options: URL.BookmarkResolutionOptions.withSecurityScope, relativeTo: nil, bookmarkDataIsStale: &isStale)
+            
+            if isStale.boolValue{
+                let bookmark = try url.bookmarkData(options: .withSecurityScope, includingResourceValuesForKeys: nil, relativeTo: nil)
+                UserDefaults.standard.setValue(bookmark, forKey:bookmarkKey(url.path!))
+            }
+            
+            if !url.startAccessingSecurityScopedResource(){
+                print("Failed to access sandbox files")
+            }
+            
+            return  FileManager.default.fileExists(atPath: url.path ?? "") &&
+                    FileManager.default.isWritableFile(atPath: url.path ?? "")
+            
         } catch {
-          print(error.localizedDescription)
+            print(error.localizedDescription)
+            return false
         }
-      }
     }
-  }
-  
-  func resolveBookmark(data: Data) -> Bool{
-    do{
-      var isStale = ObjCBool(false)
-      let url = try NSURL(resolvingBookmarkData: data, options: URL.BookmarkResolutionOptions.withSecurityScope, relativeTo: nil, bookmarkDataIsStale: &isStale)
-      
-      if isStale.boolValue{
-        let bookmark = try url.bookmarkData(options: .withSecurityScope, includingResourceValuesForKeys: nil, relativeTo: nil)
-        UserDefaults.standard.setValue(bookmark, forKey:bookmarkKey(url.path!))
-      }
-      
-      if !url.startAccessingSecurityScopedResource(){
-          print("Failed to access sandbox files")
-      }
-      
-      return true
-      
-    } catch {
-        print(error.localizedDescription)
-        return false
-    }
-  }
-
+    
 }
