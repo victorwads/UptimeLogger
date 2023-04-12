eb87514c7e5eee398be8d866c4e46cfa9e198438
Victor Wads <victor.laureano@gmail.com>
2023-04-12 00:23:16 -0300

    chore(App): close app on close window

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -24,4 +24,8 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
